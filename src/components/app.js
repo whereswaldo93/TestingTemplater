e8834b43c8ad14cae877ea4a233acccbb1f368b2
Osvaldo Saldana <osvaldo.saldana99@gmail.com>
2018-12-15 11:25:54 -0700

    Changed port and app title

--- src/components/app.js
+++ src/components/app.js
@@ -4,8 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>My Test Application</h1>
-        <h2>React Bootstrap</h2>
+        <h1>Bottega Madlibs</h1>
       </div>
     );
   }
